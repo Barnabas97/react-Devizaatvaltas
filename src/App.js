b27a9362e037af0e7ef5340a35e226ed03ede10c
Barnabas97 <barnajudak@gmail.com>
2021-05-28 18:15:57 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ export default function App() {
       <div className="inputdoboz">
         <div className="row">
           <div className="column-sm">
-            <h2>Deviza vááltás</h2>
+            <h2>Deviza váltás</h2>
           </div>
         </div>
         <div className="row">
@@ -21,18 +21,29 @@ export default function App() {
           <input type="number" name="forint" value={forint} onChange={write} />
         </div>
       </div>
-     
-        
+      <div className="outputdoboz">
+        <h3 className="euro">
+          {`${forint} HUF = `}
+          {forint !== ''
+            ? `${(parseInt(forint) / 320).toFixed(2)} EUR`
+            : '0.00 EUR'}{' '}
+        </h3>
+        <h3 className="dollar">
+          {`${forint} HUF = `}
+          {forint !== '' ? `${parseInt(forint) / 250} USD` : '0.00 USD'}
+        </h3>
+      </div>
+      <div className="infodoboz">
         <div className="row">
-          <h6>Árfolyam:</h6>
+          <h5>Árfolyam:</h5>
         </div>
-        
-        <div className="devEur row">
-          <h8>1 EUR = 320 forint</h8>
+
+        <div className="row">
+          <h8>1 EUR = 320 HUF</h8>
         </div>
-        
-        <div className="devUsd row">
-          <h8>1 USD = 250 forint</h8>
+
+        <div className="row">
+          <h8>1 USD = 250 HUF</h8>
         </div>
       </div>
     </div>
